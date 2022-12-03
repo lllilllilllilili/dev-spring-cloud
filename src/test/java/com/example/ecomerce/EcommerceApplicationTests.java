85a9952a1f6d72cddcdd65fa00faacd50ce8541b
lllilllilllilili <hik3562@hufs.ac.kr>
2022-12-04 00:57:28 +0900

    유레카 서버 세팅

---
+++ src/test/java/com/example/ecomerce/EcommerceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
